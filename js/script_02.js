6c86d8b2fb4a747f6bfcaf7f8fcadd502382a900
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-19 17:49:00 -0300

    Pequenas modificações

--- js/script_02.js
+++ js/script_02.js
@@ -3,35 +3,37 @@ const $inputName = document.querySelector('#name')
 const $inputEmail = document.querySelector('#email')
 const $inputPassword = document.querySelector('#password')
 
-const $form = document.querySelector('form')
-const $inputs = document.querySelectorAll('input')
-const $spanError = document.querySelectorAll('.input span')
+const $spanName = document.querySelector('.error-name')
+const $spanEmail = document.querySelector('.error-email')
+const $spanPassword = document.querySelector('.error-password')
 
+// const $form = document.querySelector('form')
+// const $inputs = document.querySelectorAll('input')
 
 function verifyValidate() {
 
     if (!$inputName.value) {
-        $spanError[0].classList.add('active')
-        $spanError[0].textContent = 'Campo obrigatório'
+        $spanName.classList.add('active')
+        $spanName.textContent = 'Campo obrigatório'
     } else {
-        $spanError[0].classList.remove('active')
-        $spanError[0].textContent = ''
+        $spanName.classList.remove('active')
+        $spanName.textContent = ''
     }
 
     if (!$inputEmail.value) {
-        $spanError[1].classList.add('active')
-        $spanError[1].textContent = 'Campo obrigatório'
+        $spanEmail.classList.add('active')
+        $spanEmail.textContent = 'Campo obrigatório'
     } else {
-        $spanError[1].classList.remove('active')
-        $spanError[1].textContent = ''
+        $spanEmail.classList.remove('active')
+        $spanEmail.textContent = ''
     }
 
     if (!$inputPassword.value) {
-        $spanError[2].classList.add('active')
-        $spanError[2].textContent = 'Campo obrigatório'
+        $spanPassword.classList.add('active')
+        $spanPassword.textContent = 'Campo obrigatório'
     } else {
-        $spanError[2].classList.remove('active')
-        $spanError[2].textContent = ''
+        $spanPassword.classList.remove('active')
+        $spanPassword.textContent = ''
     } 
 }
 
